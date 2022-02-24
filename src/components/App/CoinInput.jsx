cbc3beb2a9dd2e392ccca31f395d20dfde5f3f5d
Sylwia Kulka <senponadwszystko@gmail.com>
2022-02-24 11:22:13 +0100

    Modification of CoinInput, index.js; deleted redundanr files

--- src/components/App/CoinInput.jsx
+++ src/components/App/CoinInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function CoinInput(props) {
-    return (
+        return (
         <>
          <label htmlFor={props.id}>{props.description}</label>
             <input
